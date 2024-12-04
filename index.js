7ed3e8c188d8d311b2cd9bcca0e460265e2c6220
aaishwarymishra@gmail.com <aaishwarymishra@gmail.com>
2024-12-04 13:43:45 +0530

    Render hosting

--- index.js
+++ index.js
@@ -8,45 +8,16 @@ import ejs from "ejs";
 const app = express(); // Creating an instance of express
 const port = 3000; // Defining the port number
 const __dirname = import.meta.dirname; // Getting the directory name of the current module
-var clientId = '';
-var clientSecret = '';
 var accessToken = '';
-const redirectUri = 'http://localhost:3000/callback';
+const redirectUri = `${process.env.RENDER_EXTERNAL_URL}/callback`;
 
 app.use(express.static(__dirname + "/public/")); // Serving static files from the "public" directory
 app.use(cors()); // Enabling CORS for all routes
 
 // Route to serve the main HTML file
 app.get("/", (req, res) => {
-  res.render("login.ejs");
+  res.sendFile(__dirname+"/public/html/home.html"); 
 });
-
-// Route to handle login and get Spotify access token
-app.post(
-  "/login",
-  bodyParser.urlencoded({ extended: true }), // Parsing URL-encoded bodies
-  async (req, res) => {
-     clientId = req.body.client_id; // Getting client_id from the request body
-     clientSecret = req.body.secret; // Getting secret from the request body
-
-    try {
-      // Making a POST request to Spotify API to get the access token
-      let result = await axios.post(
-        "https://accounts.spotify.com/api/token",
-        {
-          grant_type: "client_credentials",
-          client_id: clientId,
-          client_secret: clientSecret,
-        },
-        { headers: { "Content-Type": "application/x-www-form-urlencoded" } },
-      );
-      res.sendFile(__dirname+"/public/html/home.html"); 
-    } catch (error) {
-      res.render("login.ejs",{err:error}); // Sending error response
-      console.error(error); // Logging the error
-    }
-  },
-);
 
 // Route to get the stored Spotify access token
 app.get('/user_login', (req, res) => {
@@ -54,7 +25,7 @@ app.get('/user_login', (req, res) => {
   const authUrl = 'https://accounts.spotify.com/authorize?' +
       querystring.stringify({
           response_type: 'code',
-          client_id: clientId,
+          client_id: process.env.CLIENT_ID,
           scope: scope,
           redirect_uri: redirectUri
       });
@@ -72,7 +43,7 @@ app.get('/callback', async (req, res) => {
           grant_type: 'authorization_code'
       }),
       headers: {
-          'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
+          'Authorization': 'Basic ' + Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'),
           'Content-Type': 'application/x-www-form-urlencoded'
       },
   };
@@ -88,6 +59,7 @@ app.get('/callback', async (req, res) => {
 });
 
 app.get("/spotify_token", (req, res) => {
+  console.log(accessToken);
   res.json({accessToken});
 });
 // Starting the server and listening on the defined port
